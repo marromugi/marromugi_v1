787800f9119201c5236fa869cc3608ffb3486ddb
marromugi <marrromugi@gmail.com>
2023-02-02 22:30:12 +0900

    環境変数の参照変更

--- src/const/auth/newt.ts
+++ src/const/auth/newt.ts
@@ -1,2 +1,2 @@
-export const NEWT_API_TOKEN = process.env.NEXT_PUBLIC_NEWT_API_TOKEN ?? ''
-export const NEWT_SPACE_ID = process.env.NEXT_PUBLIC_NEWT_SPACE_ID ?? ''
+export const NEWT_API_TOKEN = process.env.NEWT_API_TOKEN ?? ''
+export const NEWT_SPACE_ID = process.env.NEWT_SPACE_ID ?? ''
