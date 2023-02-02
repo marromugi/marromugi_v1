3f19908265d9f4764d6de57be1304d2ff22a3c1e
marromugi <marrromugi@gmail.com>
2023-02-02 22:26:24 +0900

    不要なコンポーネント削除

--- src/const/colors/types.ts
+++ src/const/colors/types.ts
@@ -2,5 +2,5 @@ import { Theme } from '@/states/theme'
 import DesignToken from '@/const/tokens/tokens'
 
 export type SemanticsColor = {
-  [key in Theme]: (typeof DesignToken.crow)[100]
+  [key in Theme]: (typeof DesignToken.kuroyuri)[100]
 }
